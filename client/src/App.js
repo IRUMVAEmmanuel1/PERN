0be72da6fb40de748e429397941aa2a7c1896fce
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-02 18:29:45 +0200

    added DATABASE

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,7 @@ function App() {
   return (
    <div>
     <h1 className="text-4xl text-center pt-2 text-green-500"> Hello Programmer</h1>
-    <h1 className="text-4xl text-center pt-2 text-green-500"> Hello Programmer</h1>
-    <h1 className="text-4xl text-center pt-2 text-green-500"> Hello Programmer</h1>
-    <h1 className="text-4xl text-center pt-2 text-green-500"> Hello Programmer</h1>
+    
 
 
    </div>
