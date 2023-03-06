b9829a62755b0fc5103e402f2bf37936bd08fa34
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-06 20:45:19 +0200

    New Features

--- server/head.js
+++ server/head.js
@@ -8,3 +8,20 @@ app.use(cors())
 app.use(express.json())
 
 
+
+//ROUTES
+
+
+// Create a todo
+app.post("/todos", async(req, res)=>{
+    try {
+        const {description} = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)",[description]);
+        res.json(newTodo)
+        
+    } catch (err) {
+        console.log(err.message)
+        
+    }
+})
+
