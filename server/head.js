74d366c2000be9a853025923139203e6081e1f55
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-06 20:48:47 +0200

    New Features

--- server/head.js
+++ server/head.js
@@ -1,43 +1,9 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-const pool = require("./db")
-//MiddleWare
+import React from 'react'
 
-app.use(cors())
-app.use(express.json())
-
-
-
-//ROUTES
-
-
-// Create a todo
-app.post("/todos", async(req, res)=>{
-    try {
-        const {description} = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)",[description]);
-        res.json(newTodo)
-        
-    } catch (err) {
-        console.log(err.message)
-        
-    }
-})
-
-//get all sensors
-try {
-    const {description} = req.body;
-    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)",[description]);
-    res.json(newTodo)
-    
-} catch (err) {
-    console.log(err.message)
-    
+function head() {
+  return (
+    <div>head</div>
+  )
 }
-});
-// get a sensor
-app.listen(5000, () =>{
-    console.log("Server has started on port 5000")
-});
 
+export default head
